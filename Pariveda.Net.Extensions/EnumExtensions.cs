c03620ba6ec1906abb56a2aa9c60b2ab683afd7b
Stephens, Nick <NStephens8191@gmail.com>
2016-10-13 18:19:35 -0500

    Added Enum Extension class

---
+++ Pariveda.Net.Extensions/EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Pariveda.Net.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum enumValue)
+        {
+            // Get the field within the enum
+            var memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
+
+            // If any members were found
+            if (memberInfo != null && memberInfo.Any())
+            {
+                // Get any description attributes on member
+                var descriptionAttributes =
+                    memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+                if (descriptionAttributes != null && descriptionAttributes.Any())
+                {
+                    return descriptionAttributes.First().Description;
+                }
+            }
+
+            // If no member found or member does not have description attribute, return the default to string value
+            return enumValue.ToString();
+        }
+    }
+}
